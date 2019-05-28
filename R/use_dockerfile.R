070aeee8f689aaf8635a8418dfcb54b3fd8a29e0
Cole Brokamp <cole.brokamp@gmail.com>
2019-05-28 11:47:31 -0400

    install specific version of dep in Dockerfile

--- R/use_dockerfile.R
+++ R/use_dockerfile.R
@@ -2,12 +2,14 @@
 ## from: list of rocker base images to start from
 
 use_dockerfile <-
-  function(from = c('R-ver', 'verse', 'shinyverse', 'spatial'), r_version = with(R.version, paste(major, minor, sep = '.'))){
+  function(
+           from = c('R-ver', 'verse', 'shinyverse', 'spatial'),
+           r_version = with(R.version, paste(major, minor, sep = '.'))
+           ){
     dockerfile_lines <-
       c(paste0('FROM ', 'rocker/', from[1], ':', r_version),
         "RUN echo \"options(repos = c(CRAN = 'https://cran.rstudio.com/'), prompt='R > ', download.file.method = 'libcurl')\" > /.Rprofile",
         "ENV R 'R --no-environ --no-site-file --no-restore --quiet --no-save'",
-        "RUN $R -e \"install.packages('pak')\"",
         "RUN $R -e \"source('https://install-github.me/cole-brokamp/dep')\"",
         "COPY . /workdir",
         "WORKDIR /app",

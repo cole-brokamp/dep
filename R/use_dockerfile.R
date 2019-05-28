4201e3256aef880da7349e511b50ae12143f53af
Cole Brokamp <cole.brokamp@gmail.com>
2019-05-28 12:06:20 -0400

    0.1.1 release

--- R/use_dockerfile.R
+++ R/use_dockerfile.R
@@ -10,7 +10,7 @@ use_dockerfile <-
       c(paste0('FROM ', 'rocker/', from[1], ':', r_version),
         "RUN echo \"options(repos = c(CRAN = 'https://cran.rstudio.com/'), prompt='R > ', download.file.method = 'libcurl')\" > /.Rprofile",
         "ENV R 'R --no-environ --no-site-file --no-restore --quiet --no-save'",
-        "RUN $R -e \"source('https://install-github.me/cole-brokamp/dep')\"",
+        "RUN $R -e \"source('https://install-github.me/cole-brokamp/dep@0.1.1')\"",
         "COPY . /workdir",
         "WORKDIR /app",
         "RUN $R -e \"dep::loy('/app')\""

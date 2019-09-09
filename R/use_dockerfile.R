01dd8229c51f5225c7b56fef8fae8380a76e932b
Cole Brokamp <cole.brokamp@gmail.com>
2019-09-09 17:03:29 -0400

    WIP dockerfile and sysreqs

--- R/use_dockerfile.R
+++ R/use_dockerfile.R
@@ -1,33 +1,73 @@
+#' Get system dependencies for R packages
+#'
+#' Calls the r-hub "sysreqs" API. Platform is currently hardcoded for Ubuntu
+#' 16.04 (\code{linux-x86_64-debian-gcc})
+#'
+#' @param pkgs a character vector of package names
+#'
+#' @export
+#' @importFrom magrittr %>%
+#' @importFrom httr GET content
+
+get_sysreqs <- function(pkgs){
+  platform <- "linux-x86_64-debian-gcc"
+  if( is.null(pkgs) ) return(NULL)
+  paste0('https://sysreqs.r-hub.io/pkg/',
+         paste(pkgs, collapse= ','),
+         '/',
+         platform) %>%
+    httr::GET() %>%
+    httr::content(type = 'application/json', as = 'parsed') %>%
+    unlist()
+}
+
+get_sysreqs(pkgs = c('magick', 'sf'))
+get_sysreqs('tidyverse')
+get_sysreqs('fortune')
+get_sysreqs(NULL)
+
+make_sysreqs_install_instructions <- function(pkgs = 'sf') {
+  sysreqs_install_lines <-
+    c(
+      "export DEBIAN_FRONTEND=noninteractive",
+      "apt-get -y update",
+      "apt-get install -y",
+      paste(get_sysreqs(pkgs))
+    )
+  paste(sysreqs_install_lines, sep = "\n")
+}
 
 ## from: list of rocker base images to start from
 
 use_dockerfile <-
   function(
            from = c('r-ver', 'verse', 'shinyverse', 'spatial'),
-           r_version = with(R.version, paste(major, minor, sep = '.'))
+           r_version = paste(getRversion(), sep = '.')
            ){
+
     dockerfile_lines <-
-      c(paste0('FROM ', 'rocker/', from[1], ':', r_version),
+      c(
+        paste0('FROM ', 'rocker/', from[1], ':', r_version),
         "RUN echo \"options(repos = c(CRAN = 'https://cran.rstudio.com/'), prompt='R > ', download.file.method = 'libcurl')\" > /.Rprofile",
         "ENV R 'R --no-environ --no-site-file --no-restore --quiet --no-save'",
         "RUN $R -e \"source('https://install-github.me/cole-brokamp/dep@0.1.1')\"",
-        "COPY DESCRIPTION /workdir/",
-        "RUN $R -e \"dep::loy('/workdir')\""
-        ## RUN useradd --create-home appuser
-        ## WORKDIR /home/appuser
-        ## USER appuser
-        "COPY . /workdir",
-        "WORKDIR /workdir",
+        ## paste('RUN', make_sysreqs_install_instructions()),
+        "COPY DESCRIPTION /code/",
+        "RUN $R -e \"dep::loy('/code')\"",
+        ## RUN useradd --create-home coder
+        ## WORKDIR /home/coder
+        ## USER coder
+        "COPY . /code",
+        "WORKDIR /code",
         "CMD [ \"R\", \"--quiet --no-save --no-restore\" ]"
-        )
-    ## TODO should the workdir used to install the project contain all the R files and data?
-    ## or should only the DESC file be copied over to bootstrap the library and then the other files will be available as a mounted drive?  (drive will have to be mounted anyway to support output files...)
+      )
 
     cat(dockerfile_lines, file = 'Dockerfile', sep = '\n')
+
   }
 
 use_docker_compose_file <-
   function(){
     ## TODO: write function that will create docker-compose file
-    ## this would autofill in the correct defaults for "docker run" and also map $PWD to /workdir in the container
+    ## this would autofill in the correct defaults for "docker run" and also map $PWD to /code in the container
     }
